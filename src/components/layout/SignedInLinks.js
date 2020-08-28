500ec138d6ee99fd8a88863abb790842b66c650e
raj patel <kakhilesh79@gmail.com>
2020-08-28 21:23:05 +0530

    after avtar changes and build directory to dist

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -9,7 +9,10 @@ const SignedInLinks = (props) => {
       <ul className="right">
         <li><NavLink to='/create'>New Project</NavLink></li>
         <li><a onClick={props.signOut}>Log Out</a></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">{props.profile.initials}</NavLink></li>
+        <li><NavLink to='/' className="btn btn-floating blue lighten-1">
+        <img src={`https://joeschmoe.io/api/v1/${props.profile.initials}`} />
+     
+        </NavLink></li>
       </ul>
     </div>
   )
